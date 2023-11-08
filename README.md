26637d5a3f1c3c17907d32d77197539886853fa9
Rinjae <dearmai@paran.com>
2023-11-08 13:12:07 +0900

    윈도우 서비스 등록 방법 추가

---
+++ README.md
@@ -0,0 +1,4 @@
+https://nssm.cc/
+
+nssm install "Socks5 Proxy Server" C:\socks5-proxy-server.exe
+nssm start "Socks5 Proxy Server"
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
@@ -8,21 +9,26 @@ import (
 )
 
 func main() {
-	// SOCKS5 서버를 설정합니다 (인증 없음)
+	// 서버 IP와 포트를 변수로 설정
+	ip := "0.0.0.0" // 모든 인터페이스에서 들어오는 연결을 수락
+	port := "1080"  // 기본 SOCKS5 포트
+
+	// SOCKS5 서버 설정 (인증 없음)
 	conf := &socks5.Config{}
 	server, err := socks5.New(conf)
 	if err != nil {
 		log.Fatalf("Failed to create SOCKS5 server: %v", err)
 	}
 
-	// TCP로 1080 포트를 열고 듣습니다
-	listener, err := net.Listen("tcp", "127.0.0.1:1080")
+	// IP와 포트를 이용하여 TCP 리스너 설정
+	address := fmt.Sprintf("%s:%s", ip, port)
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("Failed to listen on 127.0.0.1:1080: %v", err)
+		log.Fatalf("Failed to listen on %s: %v", address, err)
 	}
 
-	// 서버 시작
-	log.Println("SOCKS5 server listening on 127.0.0.1:1080")
+	// 서버 시작 정보 로깅
+	log.Printf("SOCKS5 server listening on %s\n", address)
 	if err := server.Serve(listener); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
