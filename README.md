4e431be5338fa5b6557d2440118e207a76246f5a
Rinjae <dearmai@paran.com>
2024-01-10 12:48:23 +0900

    user, password 인증 관련 코드 추가 (테스트 안함)

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ https://nssm.cc/
 
 nssm install "Socks5 Proxy Server" C:\socks5-proxy-server.exe
 nssm start "Socks5 Proxy Server"
+
+nssm edit "Socks5 Proxy Server(go)"
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -9,18 +10,28 @@ import (
 )
 
 func main() {
-	// 서버 IP와 포트를 변수로 설정
-	ip := "0.0.0.0" // 모든 인터페이스에서 들어오는 연결을 수락
-	port := "1080"  // 기본 SOCKS5 포트
+	// 기본값을 가진 포트 플래그를 정의
+	var port string
+	flag.StringVar(&port, "port", "1080", "The port number for the SOCKS5 server to listen on")
+	flag.Parse() // 플래그를 파싱합니다.
 
-	// SOCKS5 서버 설정 (인증 없음)
+	ip := "0.0.0.0" // 모든 인터페이스에서 들어오는 연결을 수락
+
+	// SOCKS5 서버 설정 (username, password 인증)
 	conf := &socks5.Config{}
+
+	//cred := socks5.StaticCredentials{
+	//	"foo": "bar",
+	//}
+	//cator := socks5.UserPassAuthenticator{Credentials: cred}
+	//conf.AuthMethods = []socks5.Authenticator{cator}
+
 	server, err := socks5.New(conf)
 	if err != nil {
 		log.Fatalf("Failed to create SOCKS5 server: %v", err)
 	}
 
-	// IP와 포트를 이용하여 TCP 리스너 설정
+	// IP와 파싱된 포트를 이용하여 TCP 리스너 설정
 	address := fmt.Sprintf("%s:%s", ip, port)
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
